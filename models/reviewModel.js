5f6fd3097b09e96557f3c366d366e670e8b9938d
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-07-26 10:56:45 +0200

    Reformat the createdAt

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -25,7 +25,15 @@ const reviewSchema = new mongoose.Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: () => {
+        const currentDate = new Date();
+
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        const day = String(currentDate.getDate()).padStart(2, "0");
+
+        return new Date(`${day}/${month}/${year}`);
+      },
     },
     author: {
       type: String,
