42cc649a4dfc03e6d3de3d11b8490594447dc62c
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-07-27 17:19:21 +0200

    Add image upload logic to patch requests

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,15 +9,12 @@ router.use("/:productId/reviews", reviewRoutes);
 router
   .route("/")
   .get(productController.getAllProducts)
-  .post(
-    productController.uploadProductImages,
-    productController.createProduct
-  );
+  .post(productController.uploadProductImages, productController.createProduct);
 
 router
   .route("/:id")
   .get(productController.getProduct)
-  .patch(productController.updateProduct)
+  .patch(productController.uploadProductImages, productController.updateProduct)
   .delete(productController.deleteProduct);
 
 module.exports = router;
